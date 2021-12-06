1d8d662bf6717145a13e0927d7f2462ca54c8bd7
Roman Vitruk <skreib@gmail.com>
2021-12-06 19:27:10 +0200

    # fix code style

--- app/View/Components/Header.php
+++ app/View/Components/Header.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class header extends Component
+class Header extends Component
 {
     /**
      * Create a new component instance.
